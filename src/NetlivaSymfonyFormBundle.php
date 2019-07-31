cf021989d165964613a2a5a693d591fe335973aa
Bilal YILMAZ <mail@bilalyilmaz.com>
2019-07-31 18:01:18 +0300

    Ajax Submit Eklendi

--- src/NetlivaSymfonyFormBundle.php
+++ src/NetlivaSymfonyFormBundle.php
@@ -1,9 +1,18 @@
 <?php
 namespace Netliva\SymfonyFormBundle;
 
+use Netliva\SymfonyFormBundle\DependencyInjection\Compiler\FormThemePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NetlivaSymfonyFormBundle extends Bundle
 {
-
+	/**
+	 * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+		$container->addCompilerPass(new FormThemePass());
+	}
 }
---
+++ src/Resources/public/netliva_ajax_submit.js
@@ -0,0 +1,88 @@
+(function ($, window) {
+
+	window.ajaxFormCreate = function()
+	{
+		$(".be_ajax_form").ajaxForm({
+			dataType: 'json',
+			success:ajaxFormSuccess
+		});
+		$(".be_ajax_form").removeClass("be_ajax_form").addClass("ajax_form");
+	};
+
+	window.ajaxFormSuccess = function (response, statusText, xhr, $form) {
+		$(".has-error").removeClass("has-error has-danger");
+		$form.find(".form-control-feedback").remove();
+		if (response.situ === 'success')
+		{
+			if(response.alert !== undefined)
+			{
+				responseAlert(response.alert.title, response.alert.text, response.alert.type, response.alert.closeTime ? response.alert.closeTime:2500 ,response.alert.refresh ? response.alert.refresh : false);
+				if (response.hideElem)
+				{
+					$('#'+response.hideElem).hide();
+				}
+			}
+			else
+			{
+				$form.before(
+					'<div class="alert alert-success" role="alert">'+
+					'<h4 class="alert-heading">'+(response.message !== undefined && response.message.title !== undefined?response.message.title:doneText)+'</h4>'+
+					'<p>'+(response.message !== undefined && response.message.content !== undefined ? response.message.content:doneText)+'</p>'+
+					'</div>'
+				);
+				if(response.removeForm === false)
+				{
+					$(".alert").slideUp(1000);
+				}
+			}
+
+			if(response.removeForm !== false)
+			{
+				$form.remove();
+			}
+
+			if(response.hideModal === true || response.hideModal === undefined)
+			{
+				$(".modal").modal("hide");
+			}
+
+			if(response.noty !== undefined)
+			{
+				$.notify(response.noty.message, response.noty.class);
+			}
+		}
+		else
+		{
+			if (response.alert !== undefined)
+			{
+				$(".modal").modal("hide");
+				responseAlert(response.alert.title, response.alert.text, response.alert.type, response.alert.closeTime ? response.alert.closeTime:2500 ,response.alert.refresh ? response.alert.refresh : false);
+
+			}
+			else
+			{
+				$.each(response.errors, function(name, err) {
+					let errorsText = '<div class="help-block form-text with-errors form-control-feedback">' +
+						'<ul class="list-unstyled">';
+
+					$.each(err, function(n, errorText) {
+						errorsText += "<li>"+errorText+"</li>";
+					});
+					errorsText += "</ul></div>";
+					$("#fg_"+name)
+						.addClass("has-error has-danger")
+						.find(".form-control").after(errorsText);
+				});
+			}
+
+		}
+		if (response.refresh) window.location.reload();
+		else if (response.url) { window.location.href = response.url; }
+		else if (response.script) { eval(response.script); }
+	};
+
+})(jQuery, window);
+
+
+$(document).ajaxComplete(ajaxFormCreate);
+jQuery(ajaxFormCreate);
