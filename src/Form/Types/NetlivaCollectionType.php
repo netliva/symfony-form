2158685ce3b0e3e7e47d4f4ce6a396cd26bac8e7
msthzn <mesuthazen@gmail.com>
2019-09-03 11:26:11 +0300

    Collection Type forma eklendi

---
+++ src/Form/Types/NetlivaCollectionType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msthzn
+ * Date: 21.07.2018
+ * Time: 10:17
+ */
+
+namespace App\Form\TypeExtension;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NetlivaCollectionType extends AbstractType
+{
+
+	public function buildView (FormView $view, FormInterface $form, array $options)
+	{
+		$view->vars['prototype_name'] = $options['prototype_name'];
+	}
+
+	public function configureOptions (OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'prototype'		=> true,
+			'allow_add'		=> true,
+			'allow_delete'	=> true,
+			'allow_extra_fields' => true,
+			'by_reference'	=> false,
+			'attr'			=> ['data-class'=>'collection_type_area'],
+		]);
+	}
+
+	public function getBlockPrefix ()
+	{
+		return 'collection_type';
+	}
+
+	public function getParent ()
+	{
+		return CollectionType::class;
+	}
+
+}
---
+++ src/Resources/public/netliva_collection.js
@@ -0,0 +1,84 @@
+(function ($, window) {
+
+	window.addFormForCollection = function ($collectionHolder, $newBtn, settings) {
+		let prototype = $collectionHolder.data('prototype');
+		let index = $collectionHolder.data('index')+1;
+
+		let rep = new RegExp(settings.prototypeName,"g");
+		let newForm = prototype.replace(rep, index);
+
+		$collectionHolder.data('index', index);
+		let $newFormLi = $('<li></li>').append(newForm);
+		$newFormLi.data('index',index);
+		if ($collectionHolder.hasClass('list-group')) $newFormLi.addClass('list-group-item');
+		addDeleteLinkCollection($newFormLi, settings.delBtnText);
+		$newBtn.before($newFormLi);
+		return $newFormLi;
+	};
+
+	window.addDeleteLinkCollection = function($tagFormLi, btnText) {
+		let $removeFormA = $('<a class="btn btn-danger" href="#">'+btnText+'</a>');
+		$tagFormLi.append($removeFormA);
+
+		$removeFormA.on('click', function(e) {
+			e.preventDefault();
+			myDialogBox({
+				id:'ForDelete',
+				title: deleteConfirmTitle,
+				content: deleteConfirmText,
+				buttons : [
+					{label: confirmButtonText, class:'danger', action:function(e) { $('#generalDialogBoxForDelete').modal('hide'); $tagFormLi.remove(); }},
+					{label: cancelButtonText, action:'close', class:'info'}
+				],
+			});
+		});
+	};
+
+	$.fn.collection = function(settings)
+	{
+		settings = $.extend({ addBtnText:addButtonText, prototypeName:'__name__', delBtnText:delButtonText, afterAction:function(){} }, settings);
+
+		let $collectionHolder = this;
+		let $addTagLink = $('<a class="btn btn-success" href="#" class="addCollectionLink">'+settings.addBtnText+'</a>');
+		let $newBtn = $('<li class="addCollBtn text-center"></li>').append($addTagLink);
+
+		$collectionHolder.find('>li').each(function() { addDeleteLinkCollection($(this), settings.delBtnText); });
+
+		if ($collectionHolder.hasClass('list-group')) $newBtn.addClass('list-group-item');
+
+		let maxIndex = 0;
+		this.find('li').each(function () {
+			if ($(this).data("index")>maxIndex) maxIndex = $(this).data("index");
+		});
+		$collectionHolder.data('index',maxIndex);
+
+		$collectionHolder.append($newBtn);
+
+		$addTagLink.on('click', function(e) {
+			// prevent the link from creating a "#" on the URL
+			e.preventDefault();
+
+			// add a new tag form (see next code block)
+			let $newForm = addFormForCollection($collectionHolder, $newBtn, settings);
+
+			settings.afterAction($newForm);
+		});
+
+	};
+
+	window.collectionPrepeare = function ($formId, $pname) {
+		function afterAddItem($addedElement, source)
+		{
+			if (typeof ['window.'+$formId+'_collect_function'] === 'function')
+			{
+				['window.'+$formId+'_collect_function']($addedElement, source);
+			}
+			$addedElement.find('.bs-datepicker').datepicker({
+				format: 'dd-mm-yyyy'
+			});
+		}
+		$('#collection_type_'+$formId).collection({addBtnText:addButtonText, prototypeName:$pname, delBtnText:'<i class="fa fa-times"></i>', afterAction:afterAddItem});
+		$("#collection_type_"+$formId+" li:not(.addCollBtn)").each(function () { afterAddItem($(this),"still"); });
+	};
+	
+})(jQuery, window);
