a92717a544134da1e52ab3be2f4c56f34aa9c760
Bilal YILMAZ <mail@bilalyilmaz.com>
2020-05-02 01:45:02 +0300

    Netliva Collection Contact Type Eklendi

---
+++ src/Form/Types/NetlivaCollectionContactType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Netliva\SymfonyFormBundle\Form\Types;
+
+use Netliva\SymfonyFormBundle\Form\Types\NetlivaContactType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NetlivaCollectionContactType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+		   'allow_extra_fields' => true,
+		   'entry_type'         => NetlivaContactType::class,
+		   'allow_add'          => true,
+		   'allow_delete'       => true,
+		   'by_reference'       => false,
+		   'label'              => 'Yeni bir iletişim bilgisi eklemek için Ekle butonunu kullanınız.'
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'netliva_collection_contact';
+    }
+
+    public function getParent()
+    {
+        return NetlivaCollectionType::class;
+    }
+}
---
+++ src/Form/Types/NetlivaContactType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Netliva\SymfonyFormBundle\Form\Types;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+/*
+use Symfony\Component\Form\Exception\FormException;
+use Netliva\FormBundle\Form\DataTransformer\EntityToPropertyTransformer;
+*/
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NetlivaContactType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array());
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'netliva_contact';
+    }
+
+    public function getParent()
+    {
+        return FormType::class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+		$builder
+			->add('type', ChoiceType::class, array('label' => 'Tip', 'choices' => array_flip(['gsm' => 'Gsm', 'phone' => 'Tel', 'fax' => 'Faks', 'email' => 'E-Posta' ])))
+			->add('content', TextType::class, array("label" => "İçerik"))
+			->add('internal', TextType::class, array("label" => "Dahili", 'required' => false))
+			->add('notification', CheckboxType::class, array("label" => "Bilgilendirme", 'required' => false))
+		;
+	}
+
+
+
+
+
+
+}
