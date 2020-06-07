8564e7e0629f12d431ba49e958a633f0caa1d7bd
Bilal YILMAZ <mail@bilalyilmaz.com>
2020-06-08 01:31:48 +0300

    Netliva Form Bundle Düzenlemeleri

--- src/Form/Types/NetlivaCollectionContactType.php
+++ src/Form/Types/NetlivaCollectionContactType.php
@@ -2,7 +2,6 @@
 
 namespace Netliva\SymfonyFormBundle\Form\Types;
 
-use Netliva\SymfonyFormBundle\Form\Types\NetlivaContactType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,14 +10,15 @@ class NetlivaCollectionContactType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-		   'allow_extra_fields' => true,
-		   'entry_type'         => NetlivaContactType::class,
-		   'allow_add'          => true,
-		   'allow_delete'       => true,
-		   'prototype_name'     => '__contact_name__',
-		   'by_reference'       => false,
-		   'label'              => 'Yeni bir iletişim bilgisi eklemek için Ekle butonunu kullanınız.'
-        ));
+			'allow_extra_fields' => true,
+			'entry_type'         => NetlivaContactType::class,
+			'allow_add'          => true,
+			'allow_delete'       => true,
+			'prototype_name'     => '__contact_name__',
+			'by_reference'       => false,
+			'js_settings'        => ["addBtnText" => "İletişim Bilgisi Ekle"],
+			'label'              => 'Yeni bir iletişim bilgisi eklemek için Ekle butonunu kullanınız.'
+		));
     }
 
     public function getBlockPrefix()
